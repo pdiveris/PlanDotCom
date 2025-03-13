9489d44faa7648cc8977a3cbdc04d0b093b3e1f1
petrosdiveris <petros@diveris.org>
2025-03-13 22:55:23 +0000

    main: Use explicitly React 18. Configured Vite, got main component rendering. Normall we'll do the compnent resolution automagically (check how Inertia does it) but no need to do this right now, right here

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+import './bootstrap';
+import { createRoot } from 'react-dom/client';
+import WelcomeGrid from "./components/WelcomeGrid.jsx";
+
+const props = {};
+const el = document.getElementById('app');
+const root = createRoot(el);
+root.render(<WelcomeGrid {...props} />);
+
---
+++ resources/js/components/WelcomeGrid.jsx
@@ -0,0 +1,5 @@
+export default function WelcomeGrid() {
+    return (
+        <div><h1>Grid.</h1></div>
+    )
+}
