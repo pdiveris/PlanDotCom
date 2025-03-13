6f248357e0cd85a28b083fb4b468623ec3a67959
petrosdiveris <petros@diveris.org>
2025-03-13 23:43:39 +0000

    main: Fix Grid layout mid / rage screen

--- resources/js/components/WelcomeGrid.jsx
+++ resources/js/components/WelcomeGrid.jsx
@@ -1,23 +1,35 @@
 export default function WelcomeGrid() {
     return (
-        <div class="flex gap-4 justify-between items-start place-items-center h-screen">
-            <div className="card card-dash bg-base-100 w-96">
-                <div className="card-body">
-                    <h2 className="card-title">Card Title</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 p-8">
+            <div className="card bg-base-100 p-6">
+                <h3 className="mb-4">Card Title</h3>
+                <p className="mb-6">Card content</p>
+                <button className="mt-auto">Action</button>
             </div>
-            <div className="card card-dash bg-base-100 w-96">
-                <div className="card-body">
-                    <h2 className="card-title">Card Title</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
+            <div className="card bg-base-100 p-6">
+                <h3 className="mb-4">Card Title</h3>
+                <p className="mb-6">Card content</p>
+                <button className="mt-auto">Action</button>
+            </div>
+            <div className="card bg-base-100 p-6">
+                <h3 className="mb-4">Card Title</h3>
+                <p className="mb-6">Card content</p>
+                <button className="mt-auto">Action</button>
+            </div>
+            <div className="card bg-base-100 p-6">
+                <h3 className="mb-4">Card Title</h3>
+                <p className="mb-6">Card content</p>
+                <button className="mt-auto">Action</button>
+            </div>
+            <div className="card bg-base-100 p-6">
+                <h3 className="mb-4">Card Title</h3>
+                <p className="mb-6">Card content</p>
+                <button className="mt-auto">Action</button>
+            </div>
+            <div className="card bg-base-100 p-6">
+                <h3 className="mb-4">Card Title</h3>
+                <p className="mb-6">Card content</p>
+                <button className="mt-auto">Action</button>
             </div>
         </div>
     )
