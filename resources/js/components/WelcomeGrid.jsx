385548f310637aa49bd9f375290167ff4f203b2c
petrosdiveris <petros@diveris.org>
2025-03-13 23:25:06 +0000

    main: Card Grid - ready to replace with Video comp now

--- resources/js/components/WelcomeGrid.jsx
+++ resources/js/components/WelcomeGrid.jsx
@@ -1,18 +1,24 @@
 export default function WelcomeGrid() {
     return (
-        <div>
-            <article className="prose lg:prose-xl">
-                <h1>Garlic bread with cheese: What the science tells us</h1>
-                <p>
-                    For years parents have espoused the health benefits of eating garlic bread with cheese to their
-                    children, with the food earning such an iconic status in our culture that kids will often dress
-                    up as warm, cheesy loaf for Halloween.
-                </p>
-                <p>
-                    But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
-                    springing up around the country.
-                </p>
-            </article>
+        <div class="flex gap-4 justify-between items-start place-items-center h-screen">
+            <div className="card card-dash bg-base-100 w-96">
+                <div className="card-body">
+                    <h2 className="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div className="card card-dash bg-base-100 w-96">
+                <div className="card-body">
+                    <h2 className="card-title">Card Title</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
