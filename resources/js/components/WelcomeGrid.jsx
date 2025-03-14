a346a47a46d74b6597a447341a5ac6d70132dc38
petrosdiveris <petros@diveris.org>
2025-03-14 00:12:19 +0000

    main: Made Player responsive. All's left to do now is make the data source dynamic (read jsn, pass array) and the tweaks required (tumbs, button etc)

--- resources/js/components/WelcomeGrid.jsx
+++ resources/js/components/WelcomeGrid.jsx
@@ -1,35 +1,31 @@
+import ReactPlayer from 'react-player'
+
 export default function WelcomeGrid() {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 p-8">
-            <div className="card bg-base-100 p-6">
-                <h3 className="mb-4">Card Title</h3>
-                <p className="mb-6">Card content</p>
-                <button className="mt-auto">Action</button>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-8">
+            <div className="card bg-base-100 p-6 player-wrapper" >
+                <ReactPlayer
+                    className='react-player'
+                    url='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+                    width='100%'
+                    height='100%'
+                />
             </div>
-            <div className="card bg-base-100 p-6">
-                <h3 className="mb-4">Card Title</h3>
-                <p className="mb-6">Card content</p>
-                <button className="mt-auto">Action</button>
+            <div className="card bg-base-100 p-6 player-wrapper" >
+                <ReactPlayer
+                    className='react-player'
+                    url='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
+                    width='100%'
+                    height='100%'
+                />
             </div>
-            <div className="card bg-base-100 p-6">
-                <h3 className="mb-4">Card Title</h3>
-                <p className="mb-6">Card content</p>
-                <button className="mt-auto">Action</button>
-            </div>
-            <div className="card bg-base-100 p-6">
-                <h3 className="mb-4">Card Title</h3>
-                <p className="mb-6">Card content</p>
-                <button className="mt-auto">Action</button>
-            </div>
-            <div className="card bg-base-100 p-6">
-                <h3 className="mb-4">Card Title</h3>
-                <p className="mb-6">Card content</p>
-                <button className="mt-auto">Action</button>
-            </div>
-            <div className="card bg-base-100 p-6">
-                <h3 className="mb-4">Card Title</h3>
-                <p className="mb-6">Card content</p>
-                <button className="mt-auto">Action</button>
+            <div className="card bg-base-100 p-6 player-wrapper" >
+                <ReactPlayer
+                    className='react-player'
+                    url='https://www.youtube.com/watch?v=LXb3EKWsInQ'
+                    width='100%'
+                    height='100%'
+                />
             </div>
         </div>
     )
