861c08467040108599314ad7d40fc15ffd50d074
petrosdiveris <petros@diveris.org>
2025-03-14 09:25:21 +0000

    main: Add the Title, authr, views etc. Needs styling. Needs the hover, autoplay, etc. It also needs a Chromr fix

--- resources/js/components/WelcomeGrid.jsx
+++ resources/js/components/WelcomeGrid.jsx
@@ -20,13 +20,17 @@ export default function WelcomeGrid() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-8">
             {videos.map((video) => (
                 <div className="card bg-base-100 p-6 player-wrapper" >
-                    <h1 className="text-white">{video.title}</h1>
                     <ReactPlayer
                         className='react-player'
                         url={video.videoUrl}
+                        light={<img src={video.thumbnailUrl} alt='Thumbnail' />}
                         width='100%'
                         height='100%'
                     />
+                    <h1 className="text-white">{video.title}</h1>
+                    <h2 className="text-white">{video.author}</h2>
+                    <h4 className="text-white">{video.views} views</h4>
+                    <h4 className="text-white">Uploaded {video.uploadTime}</h4>
                 </div>
             ))};
         </div>
