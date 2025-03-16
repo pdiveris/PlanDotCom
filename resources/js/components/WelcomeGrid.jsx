4caca70a14c9486421b0132bfbf2d02c9c3207fc
petrosdiveris <petros@diveris.org>
2025-03-16 22:24:46 +0000

    main: Added Night/Day switcher. It's a proof of concept, please see README.md

--- resources/js/components/WelcomeGrid.jsx
+++ resources/js/components/WelcomeGrid.jsx
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+import {ThemeProvider} from "../ThemeProvider.jsx";
+import {ThemeSelect} from "../ThemeSelect.jsx";
 
 export default function WelcomeGrid() {
     const [videos, setVideos] = useState([]);
@@ -43,46 +45,51 @@ export default function WelcomeGrid() {
     }
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-8">
-            {videos.map((video) => (
-                <div className="card bg-base-100 p-6 player-wrapper">
-                    <video
-                        className="h-full w-full rounded-lg"
-                        controls
-                        poster={video.thumbnailUrl}
-                        onMouseOver={event => videoOnEnter(event.target)}
-                        onMouseOut={event => videoOnLeave(event.target)}
-                    >
-                        <source
-                            src={video.videoUrl}
-                            type="video/mp4"
-                        />
-                        Your browser does not support the video tag.
-                    </video>
-                    <div className="container mx-auto p-6 grid grid-cols-1 gap-4">
-                        <div className="flex flex-col bg-white border-2 p-4">
-                            <h2 className="mb-2 font-bold text-xl">
-                                {video.title}
-                            </h2>
-                            <div className="mb-2 flex flex-wrap">
-                                <img className="w-10 h-10 rounded-full mr-4 mb-2"
-                                     src="/images/avatar.jpg"
-                                     alt="Avatar"
-                                />
-                                <h2 className="mb-2 font-bold">
-                                    {video.author}
+        <ThemeProvider>
+            <nav className="p-4">
+                <ThemeSelect />
+            </nav>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-8">
+                {videos.map((video) => (
+                    <div className="card bg-base-100 p-6 player-wrapper">
+                        <video
+                            className="h-full w-full rounded-lg"
+                            controls
+                            poster={video.thumbnailUrl}
+                            onMouseOver={event => videoOnEnter(event.target)}
+                            onMouseOut={event => videoOnLeave(event.target)}
+                        >
+                            <source
+                                src={video.videoUrl}
+                                type="video/mp4"
+                            />
+                            Your browser does not support the video tag.
+                        </video>
+                        <div className="container mx-auto p-6 grid grid-cols-1 gap-4">
+                            <div className="flex flex-col bg-inherit p-4 text-gray-500">
+                                <h2 className="mb-2 font-bold text-xl">
+                                    {video.title}
                                 </h2>
+                                <div className="mb-2 flex flex-wrap">
+                                    <img className="w-10 h-10 rounded-full mr-4 mb-2"
+                                         src="/images/avatar.jpg"
+                                         alt="Avatar"
+                                    />
+                                    <h2 className="mb-2 font-bold">
+                                        {video.author}
+                                    </h2>
+                                </div>
+                                <p className="text-md text-justify">
+                                    {video.views} views
+                                </p>
+                                <p className="text-md text-justify text-sm">
+                                    Uploaded {video.uploadTime}
+                                </p>
                             </div>
-                            <p className="text-md text-justify">
-                                {video.views} views
-                            </p>
-                            <p className="text-md text-justify text-sm">
-                                Uploaded {video.uploadTime}
-                            </p>
                         </div>
                     </div>
-                </div>
-            ))};
-        </div>
+                ))};
+            </div>
+        </ThemeProvider>
     )
 }
